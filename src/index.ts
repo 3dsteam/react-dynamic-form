80134a56395424d3cdaf0ff9c9205403df8175a6
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2025-03-17 15:12:47 +0100

    chore: add export for the IFieldGroup interface

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export type { IField } from "./models/field";
 export { EFieldType } from "./models/field";
 export type { ICondition, IConditionRule } from "./models/condition";
 export { EConditionRuleOperator } from "./models/condition";
+export type { IFieldGroup } from "./models/group";
