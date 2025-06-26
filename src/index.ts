216d7bdc63c420c883d0343be5d21b3fc97f6004
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2025-06-26 12:12:06 +0200

    fix: add exports for the field model

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 export { DynamicForm as default } from "./components/dynamic-form";
-export type { EConditionRuleOperator, ICondition, IConditionRule } from "./models/condition";
-export type * from "./models/field";
+export type { ICondition, IConditionRule } from "./models/condition";
+export { EConditionRuleOperator } from "./models/condition";
+export type {
+    IFieldType,
+    ITextField,
+    ITextAreaField,
+    INumberField,
+    IDateField,
+    IDateRangeField,
+    ISelectField,
+    IMultiSelectField,
+    IAutoCompleteField,
+    ICheckboxField,
+} from "./models/field";
+export { EFieldType } from "./models/field";
 export type { IFieldGroup } from "./models/group";
